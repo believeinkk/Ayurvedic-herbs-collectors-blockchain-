f5c8e008b56b86456149e612f702a74cef086c95
believeinkk <officialmk5308@gmail.com>
2025-10-01 01:04:34 +0530

    Update settings.py

--- ayurvedic_traceability/settings.py
+++ ayurvedic_traceability/settings.py
@@ -63,7 +63,21 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 CSRF_TRUSTED_ORIGINS = ['https://hashblock.up.railway.app']
 
